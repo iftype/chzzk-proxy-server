c4b6d69004c948820ec1b8f8f6ef5997db731f3f
iftype <iftype@naver.com>
2025-11-20 22:36:43 +0900

    feat(VideoMatchingService): Polling에서 하던 다시보기 매칭서비스 분리

---
+++ src/services/VideoMatchingService.js
@@ -0,0 +1,49 @@
+class VideoMatchingService {
+  static VIEDO_MATCH_STAND = 24 * 60 * 60 * 1000;
+
+  #liveLogService;
+  #videoService;
+
+  constructor({ liveLogService, videoService }) {
+    this.#liveLogService = liveLogService;
+    this.#videoService = videoService;
+  }
+
+  async matchAndSave(channelId) {
+    const videoDto = await this.#videoService.getVideoFromApi(channelId);
+
+    const lastLog = await this.#liveLogService.findLastLogByChannelId(channelId);
+    if (!videoDto || !lastLog) {
+      return { matched: false };
+    }
+
+    const { live_title, close_date, broadcast_session_id } = lastLog.toVideoMatch();
+    const { videoTitle, publishDate } = videoDto.toDomainFields();
+
+    console.log(live_title, close_date, broadcast_session_id);
+    console.log(videoTitle, publishDate);
+
+    const matchedTitle = live_title === videoTitle;
+    const matchedDate =
+      new Date(publishDate).getTime() - new Date(close_date).getTime() <
+      VideoMatchingService.VIEDO_MATCH_STAND;
+
+    if (!matchedTitle && !matchedDate) {
+      return { matched: false };
+    }
+
+    const videoModel = await this.#videoService.insertVideo(videoDto);
+    if (!videoModel) {
+      return { matched: false };
+    }
+    const { videoPK } = videoModel;
+
+    await this.#liveLogService.updateVideoIdBySessionId({
+      sessionId: broadcast_session_id,
+      videoPK,
+    });
+    return { matched: true };
+  }
+}
+
+export default VideoMatchingService;
