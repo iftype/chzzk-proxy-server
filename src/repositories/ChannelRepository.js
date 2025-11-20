b0c5f938fb3140acd2a4ae9d05735d9eaf1c4c49
iftype <iftype@naver.com>
2025-11-20 22:33:35 +0900

    refactor(Channel): 모델을 사용하는 구조로 변경

--- src/repositories/ChannelRepository.js
+++ src/repositories/ChannelRepository.js
@@ -1,30 +1,35 @@
-export default class ChannelRepository {
+import Channel from "../models/Channel.js";
+
+class ChannelRepository {
   pool;
+
   constructor(pool) {
     this.pool = pool;
   }
 
-  async upsertChannel({ channelId, channelName, channelImageUrl }) {
+  async upsertChannel(channel) {
     const sql = `
-      INSERT INTO CHZZK_CHANNELS (channel_id, channel_name, profile_image_url)
+      INSERT INTO CHZZK_CHANNELS (channel_id, channel_name, channel_image_url)
       VALUES ($1, $2, $3)
       ON CONFLICT (channel_id) 
       DO UPDATE SET 
-        channel_name = EXCLUDED.channel_name,
-        profile_image_url = EXCLUDED.profile_image_url,
-        updated_at = CASE 
-          WHEN 
-            CHZZK_CHANNELS.channel_name IS DISTINCT FROM EXCLUDED.channel_name OR
-            CHZZK_CHANNELS.profile_image_url IS DISTINCT FROM EXCLUDED.profile_image_url
-          THEN NOW() 
-          ELSE CHZZK_CHANNELS.updated_at 
-        END
-      RETURNING id;
-    `;
-    const binds = [channelId, channelName, channelImageUrl];
+          channel_name = EXCLUDED.channel_name,
+          channel_image_url = EXCLUDED.channel_image_url,
+          updated_at = CASE 
+              WHEN 
+                CHZZK_CHANNELS.channel_name IS DISTINCT FROM EXCLUDED.channel_name OR
+                CHZZK_CHANNELS.channel_image_url IS DISTINCT FROM EXCLUDED.channel_image_url
+              THEN NOW() 
+              ELSE CHZZK_CHANNELS.updated_at 
+              END
+      RETURNING *; 
+        `;
+    const dbData = channel.toDB();
+    const binds = [dbData.channel_id, dbData.channel_name, dbData.channel_image_url];
+
     try {
       const res = await this.pool.query(sql, binds);
-      return res.rows[0] || null; // PK 반환 위해
+      return res.rows[0] ? Channel.fromDBRow(res.rows[0]) : null;
     } catch (err) {
       console.error("[ChannelRepository] upsertChannel 실패:", err.message);
       return null;
@@ -35,39 +40,39 @@ export default class ChannelRepository {
     const sql = `SELECT * FROM CHZZK_CHANNELS`;
     try {
       const res = await this.pool.query(sql);
-      return res.rows;
+      return res.rows.map((row) => Channel.fromDBRow(row));
     } catch (err) {
       console.error("[ChannelRepository] findAll 실패:", err.message);
       return [];
     }
   }
 
-  // 채널 하나만 가져오기
-  async findByChannel({ channelId }) {
+  async findByChannelId(channelId) {
     const sql = `SELECT * FROM CHZZK_CHANNELS WHERE channel_id = $1 LIMIT 1`;
     try {
       const res = await this.pool.query(sql, [channelId]);
-      return res.rows[0] || null;
+      console.log(res.rows[0], !!res.rows[0]);
+      return res.rows[0] ? Channel.fromDBRow(res.rows[0]) : null;
     } catch (err) {
-      console.error(`[ChannelRepository] findByChannel 실패: ${err.message}`);
+      console.error(`[ChannelRepository] findByChannelId 실패: ${err.message}`);
       return null;
     }
   }
 
-  async findByPK({ channelPK }) {
+  async findByPK(channelPK) {
     const sql = `
-      SELECT 
-        id, channel_id, channel_name, profile_image_url 
-      FROM CHZZK_CHANNELS
-      WHERE id = $1
-      LIMIT 1
-    `;
+            SELECT *
+            FROM CHZZK_CHANNELS
+            WHERE id = $1
+            LIMIT 1
+        `;
     try {
       const res = await this.pool.query(sql, [channelPK]);
-      return res.rows[0] || null;
+      return res.rows[0] ? Channel.fromDBRow(res.rows[0]) : null;
     } catch (err) {
       console.error("[ChannelRepository] findByPK 실패:", err.message);
       return null;
     }
   }
 }
+export default ChannelRepository;
--- src/services/ChannelService.js
+++ src/services/ChannelService.js
@@ -1,59 +1,79 @@
 import getChzzkApiResponse from "../api/chzzk-Api.js";
+import ApiChannelDTO from "../dtos/response/ApiChannelDTO.js";
 import Channel from "../models/Channel.js";
 
 class ChannelService {
   #API_BASE_URL;
   #channelRepository;
+  #channelPKCache;
 
   constructor({ channelRepository }) {
     this.#API_BASE_URL = process.env.API_BASE_URL;
     this.#channelRepository = channelRepository;
+    this.#channelPKCache = new Map();
   }
 
-  // 다가져오기
   async getChannelAllData() {
     return await this.#channelRepository.findAll();
   }
 
   async getChannelData(channelId) {
-    return await this.#channelRepository.findByChannel({ channelId });
+    if (this.#channelPKCache.has(channelId)) {
+      return this.#channelPKCache.get(channelId);
+    }
+    const channelData = await this.#channelRepository.findByChannelId(channelId);
+    if (channelData) {
+      const { channelPK } = channelData;
+      this.#channelPKCache.set(channelId, channelPK);
+      return channelData;
+    }
+    return null;
   }
 
   async getChannelByPK(channelPK) {
-    return await this.#channelRepository.findByPK({ channelPK });
+    return await this.#channelRepository.findByPK(channelPK);
   }
 
-  async getChannelId(channelId) {
-    const channelRow = await this.#channelRepository.findByChannel({ channelId });
-    return channelRow.id;
-  }
-
-  async getOrCreateChannelId({ channelId }) {
-    const channelData = await this.getChannelData(channelId);
-
-    if (channelData?.id) {
-      return channelData.id;
+  async getChannelPK(channelId) {
+    if (this.#channelPKCache.has(channelId)) {
+      return { channelPK: this.#channelPKCache.get(channelId) };
     }
+    const { channelPK } = await this.getChannelData(channelId);
+    return channelPK ? { channelPK } : null;
+  }
+
+  async getOrCreateChannelPK(channelId) {
+    if (this.#channelPKCache.has(channelId)) {
+      const channelPK = this.#channelPKCache.get(channelId);
+      return { channelPK };
+    }
+
+    const channelData = await this.getChannelData(channelId);
+    if (channelData) {
+      return channelData;
+    }
+
     const newChannelData = await this.updateChannelState(channelId);
-    return newChannelData?.id ?? null;
+    return { channelPK: newChannelData.channelPK };
   }
 
   async updateChannelState(channelId) {
-    const channelInstance = await this.getChannel(channelId);
-    if (!(channelInstance instanceof Channel)) {
-      console.log(`[채널 서비스 Polling Error not instance of Channel]`);
-      return null;
+    const channelDTO = await this.getChannelFromApi(channelId);
+    const channel = new Channel(channelDTO.toDomainFields());
+
+    const updatedChannel = await this.#channelRepository.upsertChannel(channel);
+    if (updatedChannel) {
+      const { channelId, channelPK } = updatedChannel;
+      this.#channelPKCache.set(channelId, channelPK);
     }
-    const channelRow = channelInstance.toDB();
-    return await this.#channelRepository.upsertChannel(channelRow);
+    return null;
   }
 
-  async getChannel(channelId) {
+  async getChannelFromApi(channelId) {
     const apiUrl = `${this.#API_BASE_URL}/service/v1/channels/${channelId}`;
-    console.log(`[서비스 풀링 요청]:  ${new Date().toLocaleTimeString()} 호출 `);
-
     const resContent = await getChzzkApiResponse(apiUrl);
-    return new Channel(resContent);
+    return new ApiChannelDTO(resContent);
   }
 }
+
 export default ChannelService;
