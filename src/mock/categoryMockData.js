ae0659ae4e9ac457056b91e07685396e2d5752d8
iftype <iftype@naver.com>
2025-11-20 22:36:12 +0900

    test(MockData): 테스트용 목데이터 추가

---
+++ src/mock/categoryMockData.js
@@ -0,0 +1,18 @@
+const CATEGORY_MOCK_DATA = {
+  code: 200,
+  message: null,
+  content: {
+    categoryType: "GAME",
+    categoryId: "AION2",
+    categoryValue: "아이온2",
+    posterImageUrl:
+      "https://nng-phinf.pstatic.net/MjAyNTA1MTNfMTk0/MDAxNzQ3MTIzNjc3NzE2.9r1FscbJrBAU9TkSypedS0ZL4ArAid-oY-IPj2iEORAg.OzE-8C25Ebb_2DYUlSD_Qhm8UfsSjxatHxhOomp42uYg.PNG/%ED%8F%AC%EC%8A%A4%ED%84%B0.png",
+    openLiveCount: 467,
+    concurrentUserCount: 38483,
+    tags: ["아이온2", "MMORPG", "PC", "(주)엔씨소프트"],
+    existLounge: true,
+    following: false,
+    newCategory: true,
+    dropsCampaignList: [],
+  },
+};
